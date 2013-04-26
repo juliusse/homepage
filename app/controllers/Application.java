74038104ecf53702348b98d343467693cee1d904
Julius Seltenheim <mail@julius-seltenheim.com>
2013-04-26 09:33:55 +0200

    employments

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -57,27 +57,65 @@ public class Application extends Controller {
         if (langKey.equals("de")) {
             empl.add(new Employment(new DateTime(2012, 4, 1, 1, 1), null, "Freiberufler", "", "http://www.julius-seltenheim.com", new String[] { "Webentwicklung", "Softwareentwicklung",
                     "Datenbankentwicklung", "Beratung" }));
+            empl.add(new Employment(
+                    new DateTime(2012,10, 1, 1, 1), 
+                    null,
+                    "Teamleiter & Javaentwickler", 
+                    "Docear", 
+                    "http://docear.org", 
+                    new String[] { "Uniprojekt, Entwicklung einer Real-Time Collaborative Mindmapping Tool","Kommunikation mit Auftraggeber", "Backendentwicklung (Java, OSGI, Play Framework, Akka,...)"}));
+            empl.add(new Employment(
+                    new DateTime(2013, 2, 1, 1, 1), 
+                    new DateTime(2013, 4, 1, 1, 1),
+                    "Freiberufler", 
+                    "first fox in space", 
+                    "http://firstfox.com", 
+                    new String[] { "Planung einer Applikation", "Implementation","IT-Infrastruktur (Jenkins CI, Linux-Server)"}));
+            empl.add(new Employment(
+                    new DateTime(2012, 4, 1, 1, 1), 
+                    new DateTime(2012,11, 1, 1, 1),
+                    "Freiberufler", 
+                    "Best Ants", 
+                    "", 
+                    new String[] { "Planung einer Applikation", "Entwicklung eines Prototyps"}));
             empl.add(new Employment(new DateTime(2011, 5, 1, 1, 1), new DateTime(2011, 12, 1, 1, 1), "HTW Berlin", "Studentische Hilfskraft", "http://www.htw-berlin.de", new String[] {
                     "Entwicklung von Prototypen", "Unterstützende Arbeiten" }));
             empl.add(new Employment(new DateTime(2010, 10, 1, 1, 1), new DateTime(2011, 2, 1, 1, 1), "Canary Data Solutions Ltd", "Praktikant im Bereich \".Net Softwareentwicklung\"",
                     "http://canary.co.nz", new String[] { "Softwareentwicklung", "Kundensupport", "Administration", "Refactoring" }));
             empl.add(new Employment(new DateTime(2010, 3, 1, 1, 1), new DateTime(2010, 9, 1, 1, 1), "HTW Berlin", "Tutor für Mathematik", "http://www.htw-berlin.de", new String[] {
                     "Vorbereitung und Durchführung von 2 Tutorien pro Woche", "Beantwortung von Fragen per E-Mail" }));
-            empl.add(new Employment(new DateTime(2009, 4, 1, 1, 1), new DateTime(2009, 12, 1, 1, 1), "Café Anna Blume", "Team-Mitarbeiter", "http://www.cafe-anna-blume.de", new String[] {
-                    "vorbereiten", "servieren" }));
             empl.add(new Employment(new DateTime(2008, 6, 1, 1, 1), new DateTime(2009, 3, 1, 1, 1), "Caritas", "Zivildienst", "http://www.invia-center-berlin.de", new String[] { "Kochen", "Catering",
                     "Für Gäste sorgen", "Vorbereitung", "Servieren", "Praktikanten einweisen", "Lieferungen" }));
         } else {
             empl.add(new Employment(new DateTime(2012, 4, 1, 1, 1), null, "Freelancer", "", "http://www.julius-seltenheim.com", new String[] { "Webdevelopment", "Softwaredevelopment",
                     "Databasedevelopment", "Consulting" }));
+            empl.add(new Employment(
+                    new DateTime(2012,10, 1, 1, 1), 
+                    null,
+                    "Team Leader & Java Developer", 
+                    "Docear", 
+                    "http://docear.org", 
+                    new String[] { "University project about developping a real time mind map collaboration tool","Communication with product owner", "Backend development (Java, OSGI, Play Framework, Akka,...)"}));
+            empl.add(new Employment(
+                    new DateTime(2013, 2, 1, 1, 1), 
+                    new DateTime(2013, 4, 1, 1, 1),
+                    "Freelancer", 
+                    "first fox in space", 
+                    "http://firstfox.com", 
+                    new String[] { "Design an application based on Play Framework (Java) and MySQL", "Implementation","Set up Jenkins Continuous Integration and Linux servers"}));
+            empl.add(new Employment(
+                    new DateTime(2012, 4, 1, 1, 1), 
+                    new DateTime(2012,11, 1, 1, 1),
+                    "Freelancer", 
+                    "Best Ants", 
+                    "", 
+                    new String[] { "Design an application based on PHP, the Zend Framework and MySQL", "Start development of a prototype"}));
             empl.add(new Employment(new DateTime(2011, 5, 1, 1, 1), new DateTime(2011, 12, 1, 1, 1), "HTW Berlin", "Student Research Assistant", "http://www-en.htw-berlin.de", new String[] {
                     "Prototyping", "General Tasks" }));
             empl.add(new Employment(new DateTime(2010, 10, 1, 1, 1), new DateTime(2011, 2, 1, 1, 1), "Canary Data Solutions Ltd", ".Net Developer Intern", "http://canary.co.nz", new String[] {
                     "Software Development", "Customer Support", "Maintenance", "Refactoring" }));
             empl.add(new Employment(new DateTime(2010, 3, 1, 1, 1), new DateTime(2010, 9, 1, 1, 1), "HTW Berlin", "Tutor for Math", "http://www-en.htw-berlin.de", new String[] {
                     "Prepare and perform 2 tutorials per week", "E-mail support" }));
-            empl.add(new Employment(new DateTime(2009, 4, 1, 1, 1), new DateTime(2009, 12, 1, 1, 1), "Café Anna Blume", "Staff", "http://www.cafe-anna-blume.de",
-                    new String[] { "Preparing", "Serving" }));
             empl.add(new Employment(new DateTime(2008, 6, 1, 1, 1), new DateTime(2009, 3, 1, 1, 1), "Caritas", "Compulsory Community Service", "http://www.invia-center-berlin.de", new String[] {
                     "Cooking", "Catering", "Care for guests", "Preparing", "Serving", "Briefing Interns", "Delivery" }));
         }
@@ -94,6 +132,23 @@ public class Application extends Controller {
                     ""));
             edus.add(new Education("2001", "2008", "Hans und Hilde Coppi Gymnasium, Berlin", "http://www.coppi-gym.de/", "2.1", "", "Abitur", ""));
         }
+        
+        Collections.sort(empl, new Comparator<Employment>() {
+            @Override
+            public int compare(Employment o1, Employment o2) {
+                final DateTime end1 = o1.getTo();
+                final DateTime end2 = o2.getTo();
+                if(end1 == null && end2 == null) {
+                    return 0;
+                } else if(end1 == null) {
+                    return -1;
+                } else if(end2 == null) {
+                    return 1;
+                } else {
+                    return -end1.compareTo(end2);
+                }
+            }
+        });
 
         return ok(views.html.profile.render(empl, edus));
     }
