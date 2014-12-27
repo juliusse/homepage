bde7942f96a5d7d984ba5b42a8d4b24ee4fc0ee9
Julius Seltenheim <mail@julius-seltenheim.com>
2014-12-27 23:26:05 +0100

    update

--- ci-run.sh
+++ ci-run.sh
@@ -16,20 +16,19 @@ dpkg-buildpackage -uc -us -b
 #move debian package
 mv ../js-homepage_*.deb jenkins/builds/
 
-
 # move frontend files
-mv base_frontend/target/universal/js-homepage-frontend-*.zip jenkins/builds/
-mv base_frontend/target/scala-2.10/jacoco/jacoco.exec jenkins/jacoco/frontend.exec
+mv frontend/target/universal/js-homepage-frontend-*.zip jenkins/builds/
+mv frontend/target/scala-2.10/jacoco/jacoco.exec jenkins/jacoco/frontend.exec
 
 # remove MVC-classes that should be ignored
-rm -rf base_frontend/target/*/classes/system
-rm -rf base_frontend/target/*/classes/Routes*.class
-rm -rf base_frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/views
-rm -rf base_frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/javascript
-rm -rf base_frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/ref
-rm -rf base_frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/routes*
-rm -rf base_frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/Reverse*.class
-rm -rf base_frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/system/javascript
-rm -rf base_frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/system/ref
-rm -rf base_frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/system/routes*
-rm -rf base_frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/system/Reverse*.class
+rm -rf frontend/target/*/classes/system
+rm -rf frontend/target/*/classes/Routes*.class
+rm -rf frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/views
+rm -rf frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/javascript
+rm -rf frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/ref
+rm -rf frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/routes*
+rm -rf frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/Reverse*.class
+rm -rf frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/system/javascript
+rm -rf frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/system/ref
+rm -rf frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/system/routes*
+rm -rf frontend/target/*/classes/com/rohdeschwarz/sit/base/frontend/controllers/system/Reverse*.class
