227a6734442629561e231d4535113c0a3c37f38b
Julius Seltenheim <mail@julius-seltenheim.com>
2015-11-22 22:06:59 +0100

    update ci run file

--- ci-run.sh
+++ ci-run.sh
@@ -18,17 +18,4 @@ mv ../js-homepage_*.deb jenkins/builds/
 
 # move frontend files
 mv frontend/target/universal/js-homepage-frontend-*.zip jenkins/builds/
-mv frontend/target/scala-2.10/jacoco/jacoco.exec jenkins/jacoco/frontend.exec
-
-# remove MVC-classes that should be ignored
-rm -rf frontend/target/*/classes/system
-rm -rf frontend/target/*/classes/Routes*.class
-rm -rf frontend/target/*/classes/info/seltenheim/homepage/views
-rm -rf frontend/target/*/classes/info/seltenheim/homepage/controllers/javascript
-rm -rf frontend/target/*/classes/info/seltenheim/homepage/controllers/ref
-rm -rf frontend/target/*/classes/info/seltenheim/homepage/controllers/routes*
-rm -rf frontend/target/*/classes/info/seltenheim/homepage/controllers/Reverse*.class
-rm -rf frontend/target/*/classes/info/seltenheim/homepage/controllers/system/javascript
-rm -rf frontend/target/*/classes/info/seltenheim/homepage/controllers/system/ref
-rm -rf frontend/target/*/classes/info/seltenheim/homepage/controllers/system/routes*
-rm -rf frontend/target/*/classes/info/seltenheim/homepage/controllers/system/Reverse*.class
+mv frontend/target/scala-*/jacoco/jacoco.exec jenkins/jacoco/frontend.exec
